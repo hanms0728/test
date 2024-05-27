dfb794af39851b2d1923edc6f65ba40282330876
hanms0728 <qaz8541.ff@gmail.com>
2024-05-27 23:33:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB Atlas 연결
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://qaz8541ff:hanms5609!@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://qaz8541ff:hanms5609@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
